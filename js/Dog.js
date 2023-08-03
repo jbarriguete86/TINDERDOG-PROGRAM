8175659aa984da3a84cc4b5aa8994fc09b4570e3
jbarriguete86 <104594909+jbarriguete86@users.noreply.github.com>
2023-08-03 16:57:16 -0500

    Updated commit
    The buttons are working, need to work on render new dogs

--- js/Dog.js
+++ js/Dog.js
@@ -1,24 +1,38 @@
 import dogs from "./data.js"
+import { setSwipedStatus } from "./utils.js"
 
 class Dog {
     constructor(data){
         Object.assign(this, data)
     }
+    setSwipedStatus(){
+        if(!this.hasBeenSwiped){
+            this.hasBeenSwiped= true
+        } else {
+            this.hasBeenSwiped = false
+        }
+        console.log(`The swiped status is ${this.hasBeenSwiped}`)
+    }
+    setLikedStatus(){
+        if(!this.hasBeenLiked){
+            this.hasBeenLiked = true
+        } else{
+            this.hasBeenLiked = false
+        }
+        console.log(this.hasBeenLiked)
+        }
     getDogHtml(){
-        const {name, avatar, age, bio} = this
+        const {name, avatar, age, bio, hasBeenLiked, hasBeenSwiped} = this
+
         return `
         <img class="dog-picture" src="${avatar}" alt="">
-
-             <div class="status">
-                <img class="status-img" src="images/badge-like.png" alt="liked image">
-            </div>
-            <div class="status">
-                <img class="status-img" src="images/badge-like.png" alt="liked image">
-            </div>
-            <div id="dog-info" class="dog-info">
-                <h1 class="dog-title">${name}, ${age}</h1>
-                <h3 class="dog-bio">${bio}</h3>
-            </div> 
+        <div class="status">
+            <img id="status-img" class="${!hasBeenSwiped ? "invisible" : " "}"  src="${hasBeenLiked ? 'images/badge-like.png' : 'images/badge-nope.png' }" alt="liked image">
+        </div>
+        <div id="dog-info" class="dog-info">
+            <h1 class="dog-title">${name}, ${age}</h1>
+            <h3 class="dog-bio">${bio}</h3>
+        </div> 
             `
     }
 }
--- js/index.js
+++ js/index.js
@@ -5,15 +5,33 @@ import Dog from "./Dog.js"
 const dogContainer = document. getElementById('dog-info')
 
 
+document.addEventListener("click", e =>{
+    if(e.target.classList.contains("button-div") || e.target.classList.contains("button-selector")){
+        if(e.target.classList.contains("like")){
+            nextDog.setSwipedStatus()
+            nextDog.setLikedStatus()
+        } else if (e.target.classList.contains("dislike")){
+            nextDog.setSwipedStatus()
+        }
+            render()
+    }
+})
+
+
 
 function getNewDog(){
     const nextDogData = dogs.shift()
     return nextDogData ? new Dog(nextDogData) : "No more dogs"
 }
 
+
 function render(){
-    const nextDog = getNewDog()
     const dogContainer = document.querySelector('main').innerHTML = nextDog.getDogHtml()
 }
 
+function swiped(dog){
+    console.log(dog.hasBeenSwipped)
+}
+
+let nextDog = getNewDog() 
 render()
